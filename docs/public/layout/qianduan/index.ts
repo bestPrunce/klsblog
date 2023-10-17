3779405196519e7b2bc2d9c3be27d6c9c949ca79
lcliqizhan <1011244107@qq.com>
2023-10-17 16:05:23 +0800

    111

--- docs/public/layout/qianduan/index.ts
+++ docs/public/layout/qianduan/index.ts
@@ -2,12 +2,10 @@ import javaScriptLot from "./javaScriptLot";
 import vueLot from "./vueLot";
 import reactLot from "./reactLot";
 import regexpLot from "./regexpLot";
-import cssLot from "./cssLot";
 
 export default {
     ...javaScriptLot,
     ...vueLot,
     ...reactLot,
     ...regexpLot,
-    ...cssLot
 }
