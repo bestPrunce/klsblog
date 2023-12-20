3c6c748f212e33820fc0a6f83693bf6bf2f93dd2
lcliqizhan+2-at-583912628131 <1011244107@qq.com>
2023-12-20 14:28:37 +0800

    ts

--- docs/public/layout/system/linuxLot.ts
+++ docs/public/layout/system/linuxLot.ts
@@ -6,45 +6,89 @@ const icon = nodeIcon()
 const nIcon = nginxIcon()
 const folder = folderIcon()
 const arr = [
-    'Linux简介',
-    'CentOS7安装nodejs',
-    'CentOS7安装git',
-    'pm2启动多个node项目',
-    'CentOS7安装MySQL数据库',
+    {
+        name: 'Linux简介',
+        icon: icon
+    },
+    {
+        name: 'CentOS7安装nodejs',
+        icon: icon
+    },
+    {
+        name: 'CentOS7安装git',
+        icon: icon
+    },
+    {
+        name: 'pm2启动多个node项目',
+        icon: icon
+    },
+    {
+        name: 'CentOS7安装MySQL数据库',
+        icon: icon
+    },
     {
         name: 'Nginx',
         icon: nIcon,
-        items: ['Nginx简介', 'CentOS7安装Nginx']
+        items: [{
+            name: 'Nginx简介',
+            icon: nIcon
+        }, {
+            name: 'CentOS7安装Nginx',
+            icon: nIcon
+        }]
     },
 ]
-const ret = arr.map((item) => {
-    if(typeof item === 'string') {
-        return {
-            text: `<div style="display: flex; align-items:center">
-                        ${icon}
-                        <div style="margin-left: 6px;">${item}</div>
-                    </div>`,
-            link: `${jsPath}${item}.md`
+function formatsideBar(data: any[]) {
+    return data.map((item) => {
+        if(item.items) {
+            return {
+                text: `<div style="display: flex; align-items:center">
+                            ${folder}
+                            <div style="margin-left: 6px;">${item.name}</div>
+                        </div>`,
+                collapsed: true,
+                items: formatsideBar(item.items)
+            }
+        } else {
+            // link: `${jsPath}${item.name}/${v2}.md`,
+            return {
+                text: `<div style="display: flex; align-items:center">
+                            ${item.icon}
+                            <div style="margin-left: 6px;">${item.name}</div>
+                        </div>`,
+                link: `${jsPath}${encodeURIComponent(item.name)}.md`
+            }
         }
-    } else {
-        return {
-            text: `<div style="display: flex; align-items:center">
-                        ${folder}
-                        <div style="margin-left: 6px;">${item.name}</div>
-                    </div>`,
-            collapsed: true,
-            items: item.items.map((v2) => {
-                return {
-                    text: `<div style="display: flex; align-items:center">
-                                ${nIcon}
-                                <div style="margin-left: 6px;">${v2}</div>
-                            </div>`,
-                    link: `${jsPath}${item.name}/${v2}.md`,
-                }
-            })
-        }
-    }
-})
+    })
+}
+// const ret = arr.map((item) => {
+//     if(typeof item === 'string') {
+//         return {
+//             text: `<div style="display: flex; align-items:center">
+//                         ${icon}
+//                         <div style="margin-left: 6px;">${item}</div>
+//                     </div>`,
+//             link: `${jsPath}${item}.md`
+//         }
+//     } else {
+//         return {
+//             text: `<div style="display: flex; align-items:center">
+//                         ${folder}
+//                         <div style="margin-left: 6px;">${item.name}</div>
+//                     </div>`,
+//             collapsed: true,
+//             items: item.items.map((v2) => {
+//                 return {
+//                     text: `<div style="display: flex; align-items:center">
+//                                 ${nIcon}
+//                                 <div style="margin-left: 6px;">${v2}</div>
+//                             </div>`,
+//                     link: `${jsPath}${item.name}/${v2}.md`,
+//                 }
+//             })
+//         }
+//     }
+// })
 export default {
-    [jsPath]: ret,
+    [jsPath]: formatsideBar(arr),
 }
