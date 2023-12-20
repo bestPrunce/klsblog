4aad0bc9153bfe47c6c0a6cb2c255c6eea9c20a5
lcliqizhan+2-at-583912628131 <1011244107@qq.com>
2023-12-20 13:04:00 +0800

    加nginx

--- docs/public/layout/system/linuxLot.ts
+++ docs/public/layout/system/linuxLot.ts
@@ -1,20 +1,48 @@
 import nodeIcon from "../../svg/linuxIcon";
+import folderIcon from "../../svg/folderIcon";
+import nginxIcon from "../../svg/nginxIcon";
 const jsPath = '/system/Linux/';
 const icon = nodeIcon()
+const nIcon = nginxIcon()
+const folder = folderIcon()
 const arr = [
     'Linux简介',
     'CentOS7安装nodejs',
     'CentOS7安装git',
     'pm2启动多个node项目',
     'CentOS7安装MySQL数据库',
+    {
+        name: 'Nginx',
+        icon: nIcon,
+        items: ['Nginx简介', 'CentOS7安装Nginx']
+    },
 ]
 const ret = arr.map((item) => {
-    return {
-        text: `<div style="display: flex; align-items:center">
-                    ${icon}
-                    <div style="margin-left: 6px;">${item}</div>
-                </div>`,
-        link: `${jsPath}${item}.md`
+    if(typeof item === 'string') {
+        return {
+            text: `<div style="display: flex; align-items:center">
+                        ${icon}
+                        <div style="margin-left: 6px;">${item}</div>
+                    </div>`,
+            link: `${jsPath}${item}.md`
+        }
+    } else {
+        return {
+            text: `<div style="display: flex; align-items:center">
+                        ${folder}
+                        <div style="margin-left: 6px;">${item.name}</div>
+                    </div>`,
+            collapsed: true,
+            items: item.items.map((v2) => {
+                return {
+                    text: `<div style="display: flex; align-items:center">
+                                ${nIcon}
+                                <div style="margin-left: 6px;">${v2}</div>
+                            </div>`,
+                    link: `${jsPath}${item.name}/${v2}.md`,
+                }
+            })
+        }
     }
 })
 export default {
